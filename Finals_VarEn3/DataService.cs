ffde35ecac741de42fbf4993568cda68e2ee5a39
kolob <kolob@VLAD>
2024-06-25 11:07:00 +0700

    Добавьте файлы проекта.

---
+++ Finals_VarEn3/DataService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Finals_VarEn3.Models;
+
+
+namespace Finals_VarEn3
+{
+
+
+    public class DataService
+    {
+        private readonly string _connectionString = "Server=VLAD;Database=Zachet; Integrated Security=true;TrustServerCertificate=True;";
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            var employees = new List<Employee>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT Id, FullName, Salary, Position FROM Employee", connection);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    employees.Add(new Employee
+                    {
+                        Id = reader.GetInt32(0),
+                        FullName = reader.GetString(1),
+                        Salary = reader.GetDecimal(2),
+                        Position = reader.GetInt32(3)
+                    });
+                }
+            }
+            return employees;
+        }
+
+        public void AddUser(User user)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("INSERT INTO [User] (Login, Password, EmployeeId, RoleId) VALUES (@Login, @Password, @EmployeeId, @RoleId)", connection);
+                command.Parameters.AddWithValue("@Login", user.Login);
+                command.Parameters.AddWithValue("@Password", user.Password);
+                command.Parameters.AddWithValue("@EmployeeId", user.EmployeeId);
+                command.Parameters.AddWithValue("@RoleId", user.RoleId);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+}
---
+++ Finals_VarEn3/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Finals_VarEn3.Models;
+
+namespace Finals_VarEn3.ViewModels
+{
+
+
+    public class RegistrationViewModel : INotifyPropertyChanged
+    {
+        private readonly DataService _dataService;
+        private Employee _selectedEmployee;
+
+        public ObservableCollection<Employee> Employees { get; }
+        public ICommand RegisterCommand { get; }
+
+        public Employee SelectedEmployee
+        {
+            get => _selectedEmployee;
+            set
+            {
+                _selectedEmployee = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RegistrationViewModel()
+        {
+            _dataService = new DataService();
+            Employees = new ObservableCollection<Employee>(_dataService.GetEmployees());
+            RegisterCommand = new RelayCommand(Register, CanRegister);
+        }
+
+        private bool CanRegister(object parameter) => SelectedEmployee != null;
+
+        private void Register(object parameter)
+        {
+            var newUser = new User
+            {
+                Login = "NewUser",
+                Password = "password",
+                EmployeeId = SelectedEmployee.Id,
+                RoleId = 2 
+            };
+            _dataService.AddUser(newUser);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
